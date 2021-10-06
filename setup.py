f108f4b978bd5fe81b527d23f716f029b72b6715
Agile Geeks <agile.geeks@gmail.com>
2021-10-06 14:32:58 +0300

    Update setup.py
    Bumped version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyvieser",
-    version="0.0.3",
+    version="0.0.4",
     author="Radu Boncea",
     author_email="radu.boncea@gmail.com",
     description="A wrapper API of VIES VAT web service",
